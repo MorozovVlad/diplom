b28838a4ded082f356ec95a2d84ef6e9a3d40b72
Vlad <m_uladzislau2000@mail.ru>
2022-05-14 18:17:08 +0300

    diplom

---
+++ diplom1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace diplom1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(924, 570);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Menu;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(952, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(331, 66);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Bright";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Power";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Cost";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(952, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lamp 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1128, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 43);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Lamp 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(952, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(155, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Lamp 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(1128, 134);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(155, 43);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Lamp 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(952, 183);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(155, 43);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Lamp 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(1128, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(155, 43);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Lamp 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(952, 232);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(155, 43);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Lamp 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(1128, 232);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(155, 43);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Lamp 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(952, 407);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 43);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "Общая стоимость:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(952, 281);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(155, 43);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Lamp 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button10.Location = new System.Drawing.Point(1128, 281);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(155, 43);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Lamp 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(952, 346);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 43);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Использовано фонарей: ";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1295, 570);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ diplom1/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing.Drawing2D;
+
+namespace diplom1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection SqlConnection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class distAndPoints
+        {
+            public double dist;
+            public int point1;
+            public int point2;
+
+            public distAndPoints(double dist, int i, int j)
+            {
+                this.dist = dist;
+                this.point1 = i;
+                this.point2 = j;
+            }
+        }
+
+        int totalCost = 0;
+        int cost1 = 0;
+        int lampsCount = 0;
+        bool check = false;
+        int radius = 0;
+        int bright1 = 0;
+        List<Point> koordinat = new List<Point>();
+        List<int> brightList = new List<int>();
+
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            SqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["LampsDB"].ConnectionString);
+            SqlConnection.Open();
+
+            if (SqlConnection.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Подключение установлено!");
+            }
+
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            pictureBox1.Image = Properties.Resources.fon;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            Image image = (Image)Properties.Resources.fon;
+            Rectangle rectangle = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            graphics.DrawImage(image, rectangle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string nametable = "Lamp1";
+            data_output(sender, e, nametable);
+        }
+
+
+        private void data_output(object sender, EventArgs e, string nametable)
+        {
+            listView1.Items.Clear();
+
+            SqlDataReader dataReader = null;
+
+            try
+            {
+                string sqlCommand = "SELECT Name, Bright, Power, Cost FROM Lamps WHERE Name=@nametable";
+
+                SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["LampsDB"].ConnectionString);
+                
+                SqlCommand command = new SqlCommand(sqlCommand, connection);
+                    
+                connection.Open();
+                SqlParameter sqlParameter = new SqlParameter("@nametable", nametable);
+
+                command.Parameters.Add(sqlParameter);
+                SqlDataReader reader = command.ExecuteReader();             
+                                                   
+                ListViewItem item = null;
+
+                while (reader.Read())
+                {
+                    item = new ListViewItem(new string[] { Convert.ToString(reader["Name"]), Convert.ToString(reader["Bright"]), Convert.ToString(reader["Power"]), Convert.ToString(reader["Cost"]) });
+
+                    listView1.Items.Add(item);
+
+                    String bright = Convert.ToString(reader["Bright"]);
+
+                    bright1 = int.Parse(bright);
+
+                    String cost = Convert.ToString(reader["Cost"]);
+
+                    cost1 = int.Parse(cost);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (dataReader != null && !dataReader.IsClosed)
+                {
+                    dataReader.Close();
+                }
+            }
+            check = true;
+            brightList.Add(bright1);
+            lampsCount++;
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {           
+            if (check)
+            {
+                Point click;
+                click = e.Location;
+
+                koordinat.Add(new Point(click.X, click.Y));
+                render();
+            }
+        }
+
+        private void render()
+        {
+            
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+
+            Image image = (Image)Properties.Resources.fon;
+            Rectangle rectangle = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            graphics.DrawImage(image, rectangle);
+            for (int i = 0; i < koordinat.Count(); i++)
+            {                   
+                radius = brightList[i] / 10;
+                graphics.FillEllipse(Brushes.Orange, koordinat[i].X - radius, koordinat[i].Y - radius, radius * 2, radius * 2);
+            }
+            for (int i = 0; i < koordinat.Count(); i++)
+            {
+                radius = brightList[i] / 10;
+                graphics.FillEllipse(Brushes.Tomato, koordinat[i].X - radius * 3 / 4, koordinat[i].Y - radius * 3 / 4, radius * 3 / 2, radius * 3 / 2);
+            }
+            for (int i = 0; i < koordinat.Count(); i++)
+            {
+                radius = brightList[i] / 10;
+                graphics.FillEllipse(Brushes.Red, koordinat[i].X - radius / 2, koordinat[i].Y - radius / 2, radius, radius);
+            }
+            for (int i = 0; i < koordinat.Count(); i++)
+            {                    
+                graphics.FillEllipse(Brushes.Black, koordinat[i].X - 4, koordinat[i].Y - 4, 8, 8);
+                Pen blackPen1 = new Pen(Color.Black, 1);
+
+
+            }
+
+
+            check = false;
+            totalCost = totalCost + cost1;
+            textBox2.Text = "Использовано фонарей: " + lampsCount;
+            textBox1.Text = "Общая стоимость: " + totalCost;
+            link();
+        }
+
+        private void link()
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+
+
+            int[] components = new int[koordinat.Count()];
+            for (int i = 0; i < components.Length; i++)
+            {
+                components[i] = i + 1;
+            }
+
+
+            List<distAndPoints> dist_and_numbers = new List<distAndPoints>();
+
+            for (int i = 0; i < koordinat.Count(); i++)
+            {
+                for (int j = i + 1; j < koordinat.Count(); j++)
+                {
+                    int x1 = koordinat[i].X;
+                    int y1 = koordinat[i].Y;
+
+                    int x2 = koordinat[j].X;
+                    int y2 = koordinat[j].Y;
+
+                    double dist = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+                    dist_and_numbers.Add(new distAndPoints(dist, i, j));
+                }
+            }
+            
+            List<distAndPoints> sorted_dist_and_numbers = dist_and_numbers.OrderBy(item => item.dist).ToList();
+            int xx = 0;
+            for (int i = 0; i < sorted_dist_and_numbers.Count(); i++)
+            {
+                distAndPoints x = sorted_dist_and_numbers[i];
+
+
+                if (components[x.point1] == components[x.point2]) continue;
+                
+                Pen blackPen1 = new Pen(Color.Black, 1);
+                graphics.DrawLine(blackPen1, koordinat[x.point1].X, koordinat[x.point1].Y, koordinat[x.point2].X, koordinat[x.point2].Y);
+                xx++;
+                String w = "";
+                for (int j = 0; j < components.Count(); j++)
+                {
+                    w = w + components[j].ToString();
+
+                }
+                int color1 = components[x.point1];
+                int color2 = components[x.point2];
+                for (int j = 0; j < components.Count(); j++)
+                {
+                    if (components[j] == color1)
+                    {
+                        components[j] = color2;
+
+                    }
+                }
+                String s ="";
+                for (int j = 0; j < components.Count(); j++)
+                {
+                    s = s + components[j].ToString();
+                    
+                }
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp2";
+            data_output(sender, e, nametable);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp3";
+            data_output(sender, e, nametable);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp4";
+            data_output(sender, e, nametable);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp5";
+            data_output(sender, e, nametable);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp6";
+            data_output(sender, e, nametable);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp7";
+            data_output(sender, e, nametable);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp8";
+            data_output(sender, e, nametable);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp9";
+            data_output(sender, e, nametable);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string nametable = "Lamp10";
+            data_output(sender, e, nametable);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            Graphics graphics = pictureBox1.CreateGraphics();
+            graphics.Clear(Color.White);
+            //pictureBox1.Image = Properties.Resources.fon;
+            //pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            Image image = (Image)Properties.Resources.fon;
+            Rectangle rectangle = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+            graphics.DrawImage(image, rectangle);
+        }
+    }
+}
